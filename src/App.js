e7854d2c2aee1cf32e7f91a9d49c80e81baa6a77
shafin rahman <shafinr23@gmail.com>
2019-08-23 21:35:04 +0600

    add event lister

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ class  App extends  Component{
        {name: 'ridi' , age:38 }
      ]
    }
+   switchNameHandler = ()=>{
+     console.log('click ');
+   }
   render(){
     return(
         <div className="App">
             <h1>HI i am a react app</h1>
             <p>this is p tag</p>
-            <button> switch name  </button>
+            <button onClick={this.switchNameHandler} > switch name  </button>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
             <Person name={this.state.persons[1].name} age={this.state.persons[1].name} > my hobbies : developer </Person>
             <Person name={this.state.persons[2].name} age={this.state.persons[2].name} />
