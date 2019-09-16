8ae2027a883baaa67b39feec751633ac82d02a47
shafinr23 <shafinr23@gmail.com>
2019-09-16 15:36:46 +0600

    ADD AGAIN GIT CLASS

--- src/App.js
+++ src/App.js
@@ -17,7 +17,12 @@ class  App extends  Component{
      showPersons: false
    }
 
-
+   deletePersoneHandler = (personIndex)=>{
+      //const persons = this.state.persons.slice(); 
+      const persons = [...this.state.persons];
+      persons.splice(personIndex,1);
+      this.setState({persons:persons})
+   }
 
 
 
